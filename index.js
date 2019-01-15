37134be75dff75849e575c3671c5471b1f702ba4
Leandro <llimas@outlook.com>
2019-01-15 19:51:25 -0200

    Removendo uma API, melhorando código e deixando a saida mais bonita

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const commander = require('commander');
 
 const Apixu = require('./obj/Apixu');
-const HGWeather = require('./obj/HGWeather');
 const OpenWeatherMap = require('./obj/OpenWeatherMap');
 
 commander.version('1')
@@ -10,10 +9,22 @@ commander.version('1')
 
 let OpenWeatherMapResult = OpenWeatherMap.getOpenWeatherMap(commander.clima);
 let ApixuResult = Apixu.getApixu(commander.clima);
-let HGWeatherResult = HGWeather.getHGWeather(commander.clima);
 
-OpenWeatherMapResult.then(item => console.log(`OpenWeatherMap:`, `Cidade:`, item.city, `Temperatura:`, item.temperature + 'ºC'));
-ApixuResult.then(item => console.log(`Apixu:`, `Cidade:`, item.city, `Temperatura:`, item.temperature + 'ºC'));
-HGWeatherResult.then(item => console.log(`HGWeather:`, `Cidade:`, item.city, `Temperatura:`, item.temperature + 'ºC'));
+function changeColor(temperatura){
+    if(temperatura >20){
+        return `\x1b[33m${temperatura}\x1b[0m`;
+    } else {
+        return `\x1b[34m${temperatura}\x1b[0m`;
+    }
+}
+
+Promise.all([OpenWeatherMapResult, ApixuResult]).then((retorno) => {
+    console.log(`
+    ========================================================================================================
+    OpenWeatherMap ==> Cidade: ${retorno[0].city}, Temperatura: ${changeColor(retorno[0].temperature)} 
+    Apixu ==> Cidade: ${retorno[1].city}, Temperatura: ${changeColor(retorno[1].temperature)}          
+    ========================================================================================================
+    `);
+});
 
 
